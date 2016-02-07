c34eebe33370d024ca7677fe06ea07bfa47abe21
Mooocowgaming <rileybrayshaw@gmail.com>
2016-02-07 19:51:12 +1100

    Update 3: Code check

---
+++ Unity/Vast Space/Assets/MouseMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.RotateAround (Input.mousePosition);
+	
+	}
+}
