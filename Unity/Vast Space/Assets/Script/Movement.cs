7fc79ed32a29198f62a06d14713ad6f0239f8942
Mooocowgaming <rileybrayshaw@gmail.com>
2016-02-08 17:47:36 +1100

    Update 5 Got Movement set up.
    WE CAN MOVE

---
+++ Unity/Vast Space/Assets/Script/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float moveSpeed = 8.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate (0f, moveSpeed * Input.GetAxis ("Vertical") * Time.deltaTime, 0f);
+	
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+	}
+}
+
