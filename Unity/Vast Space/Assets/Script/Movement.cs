277d02b0fb218d2b4af1aa8092f3fcd1b69a414d
Mooocowgaming <rileybrayshaw@gmail.com>
2016-02-11 20:49:07 +1100

    Update 8: Bullets and SPEED

--- Unity/Vast Space/Assets/Script/Movement.cs
+++ Unity/Vast Space/Assets/Script/Movement.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 	public float moveSpeed = 8.0f;
+	public float boostSpeed = 16.0f;
+	[HideInInspector]
+	public Vector2 recoil = Vector2.zero;
 	// Use this for initialization
 	void Start () {
 	
@@ -15,6 +18,9 @@ public class Movement : MonoBehaviour {
 	
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+
+			transform.Translate (0f, boostSpeed * Input.GetAxis ("Boost") * Time.deltaTime, 0f);
+		}
 	}
-}
+
 
