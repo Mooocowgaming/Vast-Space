f98772964d65d804e9710ce333a25af73c0c8741
Mooocowgaming <rileybrayshaw@gmail.com>
2016-03-03 21:20:38 +1100

    Update 10: Damage Enemy Code
    Doesn’t work properly. But, you know. Its a good try.

--- Unity/Vast Space/Assets/Script/Damage.cs
+++ Unity/Vast Space/Assets/Script/Damage.cs
@@ -2,18 +2,29 @@
 using System.Collections;
 
 public class Damage : MonoBehaviour {
+	public int playerHealth = 100;
+	public int enemyHealth = 100;
+	public GameObject Enemy1;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
-	void OnCollisionEnter2D(Collision2D coll)
+	void OnCollisionEnter2D(Collision2D coll) //Enemy
 	{
 		if (coll.gameObject.name == "Orange")
 		{
-			Debug.LogError ("Hit!");
+			Debug.LogError ("Hit! -10HP");
+			enemyHealth -= 10;	
+			Debug.LogError (enemyHealth);
+
+		}
+			
+		if (enemyHealth <= 0) {
+			Debug.LogError ("Enemy Is Dead");
+			Destroy (Enemy1);
+	
 		}
 	}
 }
---
+++ Unity/Vast Space/Assets/Script/DestoryEnemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestoryEnemy : MonoBehaviour {
+	//public GameObject Enemy1;
+
+	//private Damage Damage;
+	// Use this for initialization
+	void Awake () {
+		//Damage = GetComponent<Damage>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Damage.health <= 0) {
+		//	Debug.LogError ("Enemy Is Dead");
+		//	Destroy (Enemy1);
+		//	Debug.LogError (Damage.health + "EnemyCall");
+
+
+
+		}
+	}
